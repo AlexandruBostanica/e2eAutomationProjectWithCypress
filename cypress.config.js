d0520518f0814e47abda73167ad8707cf98fef16
AlexandruBostanica <alexandru_bostanica@yahoo.com>
2025-12-09 08:17:07 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
+  screenshotOnRunFailure: true,
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'E2E Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+
   e2e: {
     baseUrl: 'https://playground.bondaracademy.com/',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    }
   },
+
   viewportWidth: 1280,
   viewportHeight: 720
 });
