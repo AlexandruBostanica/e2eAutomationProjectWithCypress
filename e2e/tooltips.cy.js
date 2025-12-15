238188b72945f4fd811395d4536a409748292272
AlexandruBostanica <alexandru_bostanica@yahoo.com>
2025-12-15 15:47:54 +0200

    Refactor tooltip tests for clarity and structure

--- e2e/tooltips.cy.js
+++ e2e/tooltips.cy.js
@@ -10,14 +10,19 @@ beforeEach('Open application', () => {
 describe('Tooltip Visibility', () => {
 
     it('Shows tooltip text on hover', () => {
+        
     navigateTo.tooltipPage()
+        
     cy.contains('Show Tooltip').trigger('mouseenter')
     cy.get('#cdk-overlay-0').contains('This is a tooltip').should('be.visible')
     })
 
     it('Displays tooltip icon on hover with SVG visibility', () => {
+        
     navigateTo.tooltipPage()
+        
     cy.get('[nbtooltipicon="alert-triangle"]').trigger('mouseenter')
     cy.get('#cdk-overlay-0').find('svg').should('be.visible')
     })
+
 })
