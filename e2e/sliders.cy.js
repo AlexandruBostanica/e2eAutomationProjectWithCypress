e7914fddace74696c588fce6ccb2c44f0a715edb
AlexandruBostanica <alexandru_bostanica@yahoo.com>
2025-12-15 15:45:36 +0200

    Update sliders.cy.js

--- e2e/sliders.cy.js
+++ e2e/sliders.cy.js
@@ -10,11 +10,11 @@ beforeEach('Open application', () => {
 describe('Slider control interaction', () => {
 
     it('Adjusts temperature slider to specific value and verifies display', () => {
-    cy.visit('https://playground.bondaracademy.com/')
     cy.get('[tabtitle="Temperature"] circle')
     .invoke('attr', 'cx', '267.66')
     .invoke('attr', 'cy', '170.58')
     .click()
     cy.get('[class="value temperature h1"]').should('contain.text', '28')
 })
+
 })
